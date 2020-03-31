50bc22382f19997944cbe591cd7cddeddef24332
Zhengy1995 <1752551862@qq.com>
2020-03-31 14:16:22 +0800

    Update draw.js

--- draw.js
+++ draw.js
@@ -58,7 +58,7 @@ export default class Draw {
 	}
 	clearCanvas() {
 		this.stopOperation()
-		this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height)
+		this.ctx.clearRect(0, 0, this.canvas.width,this.canvas.height)
 		this.ctx.fillStyle = '#fff'
     	this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
 	}
