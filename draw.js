89e2e6e682be10f57e57916b318fbcfd0a02164f
Zhengy1995 <1752551862@qq.com>
2020-03-31 14:29:15 +0800

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,6 +1,22 @@
 export default class Draw {
-	constructor(element) {
-		const parent = document.getElementById(element)
+	constructor(pid) {
+		const { parent, canvas, operDiv } = this.getDom(pid)
+		this.canvas = canvas
+		this.parent = parent
+		this.operDiv = operDiv
+		this.ctx = canvas.getContext('2d')
+		this.moveDrawfn = this.moveDrawfn.bind(this)
+		this.startDrawnfn = this.startDrawnfn.bind(this)
+		this.moveEraserfn = this.moveEraserfn.bind(this)
+		this.startEraserfn = this.startEraserfn.bind(this)
+	}
+	/**
+	 * [getDom getDom generate necessary dom element]
+	 * @param  {[string]} pid [parent id]
+	 * @return {[Object]}         [necessary dom element]
+	 */
+	getDom(pid) {
+		const parent = document.getElementById(pid)
 		const canvas = document.createElement('canvas')
 		const operDiv = document.createElement('div')
 		canvas.innerText = '您使用的浏览器不支持绘图，请使用chrome'
@@ -11,20 +27,36 @@ export default class Draw {
 		operDiv.setAttribute('style', `position:relative;width: 100%;height: 100%;top: -${height}px;left: 0;`)
 		parent.appendChild(canvas)
 		parent.appendChild(operDiv)
-		this.canvas = canvas
-		this.parent = parent
-		this.ctx = canvas.getContext('2d')
-		this.operDiv = operDiv
-		this.moveDrawfn = this.moveDrawfn.bind(this)
-		this.startDrawnfn = this.startDrawnfn.bind(this)
-		this.moveEraserfn = this.moveEraserfn.bind(this)
-		this.startEraserfn = this.startEraserfn.bind(this)
+		return { parent, canvas, operDiv }
 	}
+	/**
+	 * [startDrawing start drawing line]
+	 * @return {[void]}
+	 */
 	startDrawing() {
 		this.stopOperation()
 		this.ctx.beginPath()
 		this.operDiv.addEventListener('touchstart', this.startDrawnfn)
 		this.operDiv.addEventListener('touchmove', this.moveDrawfn)
+	}
+	startEraser() {
+		this.stopOperation()
+		this.operDiv.addEventListener('touchstart', this.startEraserfn)
+		this.operDiv.addEventListener('touchmove', this.moveEraserfn)
+	}
+	clearCanvas() {
+		this.stopOperation()
+		this.ctx.clearRect(0, 0, this.canvas.width,this.canvas.height)
+		this.ctx.fillStyle = '#fff'
+    	this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+	}
+	stopOperation() {
+		//drawing event
+		this.operDiv.removeEventListener('touchmove', this.moveDrawfn)
+		this.operDiv.removeEventListener('touchstart', this.startDrawnfn)
+		//eraser event
+		this.operDiv.removeEventListener('touchstart', this.startEraserfn)
+		this.operDiv.removeEventListener('touchmove', this.moveEraserfn)
 	}
 	startDrawnfn(e) {
 		e.stopPropagation()
@@ -47,25 +79,6 @@ export default class Draw {
 	moveEraserfn(e) {
 		const { clientX, clientY } = e.targetTouches[0]
 		this.ctx.clearRect(clientX-5, clientY-5, 10, 10)
-	}
-	stopOperation() {
-		//drawing event
-		this.operDiv.removeEventListener('touchmove', this.moveDrawfn)
-		this.operDiv.removeEventListener('touchstart', this.startDrawnfn)
-		//eraser event
-		this.operDiv.removeEventListener('touchstart', this.startEraserfn)
-		this.operDiv.removeEventListener('touchmove', this.moveEraserfn)
-	}
-	clearCanvas() {
-		this.stopOperation()
-		this.ctx.clearRect(0, 0, this.canvas.width,this.canvas.height)
-		this.ctx.fillStyle = '#fff'
-    		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
-	}
-	startEraser() {
-		this.stopOperation()
-		this.operDiv.addEventListener('touchstart', this.startEraserfn)
-		this.operDiv.addEventListener('touchmove', this.moveEraserfn)
 	}
 
 }
